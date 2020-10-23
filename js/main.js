691a308b91152b253286034cdb21abb849861e92
lerese58 <lerese5858>
2020-10-23 08:43:39 +0300

    add offline handling

--- js/main.js
+++ js/main.js
@@ -12,3 +12,10 @@ const refreshBtn = document.querySelector('.refresh-btn')
 refreshBtn.addEventListener('click', updateLocalWeather)
 const favCityForm = document.querySelector('form.fav-search')
 favCityForm.addEventListener('submit', addToFavorites)
+
+function handleOffline() {
+    if (!navigator.onLine)
+        alert('Connection lost. Please check your connection')
+}
+
+window.addEventListener('offline', handleOffline)
