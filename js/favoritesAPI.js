2ab614614c28af77b402b3a2bfcd2451ff36cc24
lerese58 <lerese58@gmail.com>
2020-12-20 00:17:35 +0300

    public

--- js/favoritesAPI.js
+++ js/favoritesAPI.js
@@ -3,7 +3,7 @@ class FavoritesAPI {
      * @returns {Promise<Array>}
      */
     async getList() {
-        const response = await fetch(`http://127.0.0.1:8088/favorites`, {
+        const response = await fetch(`http://38147d01e776.ngrok.io/favorites`, {
             method: 'GET',
         })
         return await response.json()
@@ -14,7 +14,7 @@ class FavoritesAPI {
      * @returns {Promise<Response>}
      */
     async addCity(cityName) {
-        return await fetch(`http://127.0.0.1:8088/favorites`, {
+        return await fetch(`http://38147d01e776.ngrok.io/favorites`, {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({cityToAdd: cityName})
@@ -26,7 +26,7 @@ class FavoritesAPI {
      * @returns {Promise<Response>}
      */
     async deleteCity(cityName) {
-        return await fetch(`http://127.0.0.1:8088/favorites`, {
+        return await fetch(`http://38147d01e776.ngrok.io/favorites`, {
             method: 'DELETE',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({cityToDelete: cityName})
